0fdde2ed469d6fdd7930d7992886c738daca0f36
Gabriel <gabrieltroop27@gmail.com>
2024-12-08 12:10:56 -0300

    tiredMr condicionais e ponto-virgula linhas 7 e 9

--- Teste.js
+++ Teste.js
@@ -4,8 +4,15 @@ let kaguyo;
 kaguyo = 1;
 
 if (kaguyo == 1) {
-    console.log("Kaguyo é 1")
+    console.log("Kaguyo é 1");
 }else{
-    console.log("Kaguyo não é 1")
+    console.log("Kaguyo não é 1");
 }
 console.log("abdsdjklns");
+
+let tiredMr = "Yuu";
+if (tiredMr != "Yuu"){
+    console.log("tiredMr é um impostor");
+} else {
+    console.log("tiredMr é o yuuzinho :D");
+}
