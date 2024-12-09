4ca3deca3556b63e90b21bd7a48b071e48208681
Gabriel <gabrieltroop27@gmail.com>
2024-12-09 03:20:20 -0300

    bug condicional resolvido

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const PORT = 2000;
 
 const server = http.createServer((req, res) => {
     let filePath;
-    let getIn = true;
+    
     if (req.url === '/') {
         filePath = path.join(__dirname, 'index.html');
     } else if (req.url === '/login') {
         filePath = path.join(__dirname, 'login.html');
     } else {
         filePath = path.join(__dirname, req.url);
-        getIn = false;
     }
+
     const extname = path.extname(filePath);
 
     const traduzirContentType = {
@@ -27,25 +27,24 @@ const server = http.createServer((req, res) => {
         '.gif': 'image/gif',
         '.json': 'application/json'
     };
-    
     const contentType = traduzirContentType[extname] || 'application/octet-stream';
-    if (filePath != undefined){
-        fs.readFile(filePath, function(error, data){
-            if (error){
-                res.writeHead(404);
-                res.write(`ERRO: ${error}`);
-            } else {
-                res.writeHead(200, { 'Content-Type': contentType });
-                res.end(data);
-            }
-        })
-    } else res.end('URL sem resposta definida');
-})
+    
+    fs.readFile(filePath, function(error, data) {
+        if (error) {
+            res.writeHead(404);
+            res.end(`ERRO: ${error}`);
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(data);
+        }
+    });
+});
+
 server.listen(PORT, function(error) {
-    if (error){
+    if (error) {
         console.log('ERRO: ', error);
     } else {
         console.log(`Servidor ouvindo em: http://localhost:${PORT}`);
         console.log("Para encerrar servidor: Ctrl + C");
     }
-})
+});
