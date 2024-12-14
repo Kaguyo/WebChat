34f42e5475eb1ab8e135282b1ef200ef1a369349
TiredMr <ffabricio047@gmail.com>
2024-12-14 14:20:32 -0300

    SQL AGAIN

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,9 @@
-﻿using System.Net;
+﻿using System.Data;
+using System.Data.SQLite;
+using System.Net;
 using System.Text;
 using System.Text.Json;
+using static Program;
 
 
 public class Program
@@ -57,6 +60,9 @@ public class Program
                             Console.WriteLine($"Number: {user.Number}");
                             Console.WriteLine($"Password: {user.Password}");
                             Console.WriteLine($"Password2: {user.Password2}");
+                            DalHelper.CriarBancoSQLite();
+                            DalHelper.CriarTabelaSQLite();
+                            DalHelper.Add(user);
                             byte[] buffer = Encoding.UTF8.GetBytes("Dados recebidos com sucesso");
                             response.OutputStream.Write(buffer, 0, buffer.Length);
                         }
@@ -77,10 +83,129 @@ public class Program
     }
     public class User
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string? Nome { get; set; }
         public string? Number { get; set; }
         public string? Password { get; set; }
         public string? Password2 { get; set; }
     }
+}
+
+public class DalHelper()
+    {
+        private static SQLiteConnection sqliteConnection;
+
+        // public DalHelper(){}
+
+
+        private static SQLiteConnection DbConnection(){
+            sqliteConnection = new SQLiteConnection("Data Source=C:\\Users\\Fabricio\\Codes\\Project_Conta\\WebChat\\xDataBase\\Users.sqlite");
+            sqliteConnection.Open();
+            return sqliteConnection;
+        }
+
+        public static void CriarBancoSQLite(){
+            try{
+                SQLiteConnection.CreateFile(@"C:\Users\Fabricio\Codes\Project_Conta\WebChat\xDataBase\Users.sqlite");
+            }
+            catch{
+                throw;
+            }
+        }
+        
+        public static void CriarTabelaSQLite(){
+            try{
+                using(var cmd = DbConnection().CreateCommand()){
+                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS Users(Id int, Name Varchar(50), Number Varchar(50), Password Varchar(50), Password2 Varchar(50))";
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch(Exception ex){
+                throw ex;
+            }
+        }
+
+        public static DataTable GetUsers(){
+            SQLiteDataAdapter da = null;
+            DataTable dt = new DataTable();
+            try{
+                using (var cmd = DbConnection().CreateCommand()){
+                    cmd.CommandText = "SELECT * FROM Users";
+                    da = new SQLiteDataAdapter(cmd.CommandText, DbConnection());
+                    da.Fill(dt);
+                    return dt;
+                }
+            }
+            catch (Exception ex){
+                throw ex;
+            }
+        }
+
+        public static DataTable GetUser(int Id){
+            SQLiteDataAdapter da = null;
+            DataTable dt = new DataTable();
+            try
+            {
+                using (var cmd = DbConnection().CreateCommand()){
+                    cmd.CommandText = "SELECT * FROM Users Where Id=" + Id;
+                    da = new SQLiteDataAdapter(cmd.CommandText, DbConnection());
+                    da.Fill(dt);
+                    return dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static void Add(User user){
+            try{
+                using (var cmd = DbConnection().CreateCommand()){
+                    cmd.CommandText = "INSERT INTO Users(Id, Name, Number, Password, Password2) values (@Id, @Nome, @Number, @Password, @Password2)";
+                    cmd.Parameters.AddWithValue("@Id", user.Id);
+                    cmd.Parameters.AddWithValue("@Nome", user.Nome);
+                    cmd.Parameters.AddWithValue("@Number", user.Number);
+                    cmd.Parameters.AddWithValue("@Password", user.Password);
+                    cmd.Parameters.AddWithValue("@Password2", user.Password2);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch(Exception ex){
+                throw ex;
+            }
+        }
+
+        public static void Update(User user){
+            try {
+                using (var cmd = new SQLiteCommand(DbConnection())){
+                    if (user.Id != null){
+                        cmd.CommandText = "UPDATE Users SET Name=@Name, Number=@Number, Password=@Password WHERE Id=@Id";
+                        cmd.Parameters.AddWithValue("@Id", user.Id);
+                        cmd.Parameters.AddWithValue("@Name", user.Nome);
+                        cmd.Parameters.AddWithValue("@Number", user.Number);
+                        cmd.Parameters.AddWithValue("@Password", user.Password);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex){
+                throw ex;
+            }
+        }
+
+        public static void Delete(int Id){
+            try {
+                using(var cmd = new SQLiteCommand(DbConnection())){
+                    cmd.CommandText="DELETE FROM Users WHERE Id=@Id";
+                    cmd.Parameters.AddWithValue("@Id", Id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch(Exception ex){
+                throw ex;
+            }
+        }
+
+
 }
